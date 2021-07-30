3d938757afdb6e83a988fe08ab0193f77bfb0292
mwesigwajosh <joshuamj59@gmail.com>
2021-07-30 13:57:03 +0300

    commulative build

---
+++ resources/views/login.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <title>CTFU</title>
+</head>
+<style>
+  .row, .align-items-center, .h-100{
+    position: absolute;
+    left: 0;
+    right: 0;
+  }
+</style>
+<body>
+  <div class="container h-100">
+    <div class="row align-items-center h-100">
+      <div class="col-6 mx-auto">
+        <div class="card">
+          <div class="card-header">
+            <h3>Login</h3>
+          </div>
+          <div class="card-body">
+            <form action="">
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+              <div>
+
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
